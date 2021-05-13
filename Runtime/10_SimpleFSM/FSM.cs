159b322ac1d8139158a3f2d846ba27b15fc37a8b
HalfLobsterMan <766439226@qq.com>
2021-05-13 23:21:56 +0800

    优化目录结构

---
+++ Runtime/10_SimpleFSM/FSM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CZToolKit.Core.SimpleFSM
+{
+    public class FSM
+    {
+        private Dictionary<string, IFSMState> states = new Dictionary<string, IFSMState>();
+        private IFSMState currentState;
+
+        public void Update()
+        {
+            if (currentState != null)
+                currentState.OnUpdate();
+        }
+
+        public void PushState(string stateName, IFSMState state)
+        {
+            states[stateName] = state;
+        }
+
+        public void ChangeTo(string stateName)
+        {
+            if (currentState == states[stateName])
+                return;
+
+            if (currentState != null)
+                currentState.OnExit();
+
+            currentState = states[stateName];
+            if (currentState != null)
+                currentState.OnStart();
+        }
+    }
+
+    public interface IFSMState
+    {
+        void OnStart();
+
+        void OnUpdate();
+
+        void OnExit();
+    }
+}
