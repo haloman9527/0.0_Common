ee25d65d46a2d2064e6e350e20505a2f92f53072
HalfLobsterMan <766439226@qq.com>
2021-07-31 02:13:48 +0800

    增加一个虚拟滚动列表绘制

--- Editor/EditorExtension/EditorGUILayoutExtension.cs
+++ Editor/EditorExtension/EditorGUILayoutExtension.cs
@@ -13,6 +13,7 @@
  *
  */
 #endregion
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -28,6 +29,68 @@ namespace CZToolKit.Core.Editors
         public static void EndBox()
         {
             EditorGUILayout.EndVertical();
+        }
+
+        public static float ScrollList(SerializedProperty _list, float _scroll, ref bool _foldout, int _count = 10)
+        {
+            _foldout = EditorGUILayout.Foldout(_foldout, _list.displayName, true);
+
+            if (_foldout)
+            {
+                EditorGUI.indentLevel++;
+
+                GUILayout.BeginHorizontal();
+                int size = EditorGUILayout.DelayedIntField("Count", _list.arraySize);
+                EditorGUI.indentLevel--;
+                int targetIndex = -1;
+                targetIndex = EditorGUILayout.DelayedIntField(targetIndex, GUILayout.Width(40));
+                GUILayout.EndHorizontal();
+
+                EditorGUI.indentLevel++;
+
+                if (size != _list.arraySize)
+                    _list.arraySize = size;
+
+                GUILayout.BeginHorizontal();
+                Rect r = EditorGUILayout.BeginVertical();
+
+                if (_list.arraySize > _count)
+                {
+                    int startIndex = Mathf.CeilToInt(_list.arraySize * _scroll);
+                    startIndex = Mathf.Max(0, startIndex);
+                    for (int i = startIndex; i < startIndex + _count; i++)
+                    {
+                        EditorGUILayout.PropertyField(_list.GetArrayElementAtIndex(i));
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < _list.arraySize; i++)
+                    {
+                        EditorGUILayout.PropertyField(_list.GetArrayElementAtIndex(i));
+                    }
+                }
+
+                EditorGUILayout.EndVertical();
+                GUILayout.Space(20);
+                GUILayout.EndHorizontal();
+                EditorGUI.indentLevel--;
+
+                if (Event.current.type == EventType.ScrollWheel && r.Contains(Event.current.mousePosition))
+                {
+                    _scroll += Event.current.delta.y * 0.01f;
+                    Event.current.Use();
+                }
+                if (targetIndex != -1)
+                {
+                    _scroll = Mathf.Clamp01((float)targetIndex / _list.arraySize);
+                }
+
+                r.xMin += r.width + 5;
+                r.width = 20;
+                _scroll = GUI.VerticalScrollbar(r, _scroll, (float)_count / _list.arraySize, 0, 1);
+            }
+            return _scroll;
         }
 
         public static bool BeginToggleGroup(string _label, bool _foldout, ref bool _enable)
