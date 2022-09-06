c2cd5c3ed4014fa9e67dfaa4a470b03e3f53a3af
HalfLobsterMan <766439226@qq.com>
2022-09-07 01:22:07 +0800

    剥离ViewModelFactory和ViewModelAttribute

---
+++ Modules/IntegratedViewModel/Attributes/ViewModelAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CZToolKit.Core.ViewModel
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ViewModelAttribute : Attribute
+    {
+        public Type targetType;
+
+        public ViewModelAttribute(Type targetType)
+        {
+            this.targetType = targetType;
+        }
+    }
+}
