8a5c6f740249abcf59086c610d07e88a94e13059
haloman9527 <haloman9527@qq.com>
2025-09-21 01:28:31 +0800

    update

--- Atom.GameModule/GameModuleEntry.cs
+++ Atom.GameModule/GameModuleEntry.cs
@@ -7,6 +7,7 @@ namespace Atom
     {
         private static readonly List<IGameModule> s_GameModules = new List<IGameModule>();
         private static readonly Dictionary<string, IGameModule> s_GameModulesByName = new Dictionary<string, IGameModule>();
+        private static readonly Dictionary<IGameModule, string> s_GameModuleNames = new Dictionary<IGameModule, string>();
 
         public static IGameModule GetGameModule(string name)
         {
@@ -22,14 +23,39 @@ namespace Atom
 
         public static void RegisterGameModule<T>(string name, T module) where T : class, IGameModule
         {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+
+            if (module == null)
+                throw new ArgumentNullException(nameof(module));
+
             if (s_GameModulesByName.ContainsKey(name))
-            {
-                throw new Exception($"GameModule {name} already exists");
-            }
+                throw new Exception($"GameModule {name} is already registered");
+
+            if (s_GameModuleNames.ContainsKey(module))
+                throw new Exception($"GameModule {module} is already registered");
 
             s_GameModules.Add(module);
             s_GameModulesByName.Add(name, module);
+            s_GameModuleNames.Add(module, name);
             module.Init();
+        }
+
+        public static void UnRegisterGameModule(IGameModule module)
+        {
+            if (!s_GameModuleNames.TryGetValue(module, out var name))
+                return;
+
+            try
+            {
+                module.UnInit();
+            }
+            finally
+            {
+                s_GameModules.Remove(module);
+                s_GameModuleNames.Remove(module);
+                s_GameModulesByName.Remove(name);
+            }
         }
 
         public static void Shutdown()
@@ -37,7 +63,16 @@ namespace Atom
             var count = s_GameModules.Count;
             for (var i = 0; i < count; i++)
             {
-                s_GameModules[i].UnInit();
+                try
+                {
+                    s_GameModules[i].UnInit();
+                }
+                catch (Exception e)
+                {
+#if UNITY_EDITOR
+                    UnityEngine.Debug.LogException(e);
+#endif
+                }
             }
 
             s_GameModules.Clear();
@@ -49,7 +84,16 @@ namespace Atom
             var count = s_GameModules.Count;
             for (var i = 0; i < count; i++)
             {
-                s_GameModules[i].FixedUpdate();
+                try
+                {
+                    s_GameModules[i].FixedUpdate();
+                }
+                catch (Exception e)
+                {
+#if UNITY_EDITOR
+                    UnityEngine.Debug.LogException(e);
+#endif
+                }
             }
         }
 
@@ -58,7 +102,16 @@ namespace Atom
             var count = s_GameModules.Count;
             for (var i = 0; i < count; i++)
             {
-                s_GameModules[i].Update();
+                try
+                {
+                    s_GameModules[i].Update();
+                }
+                catch (Exception e)
+                {
+#if UNITY_EDITOR
+                    UnityEngine.Debug.LogException(e);
+#endif
+                }
             }
         }
 
@@ -67,7 +120,16 @@ namespace Atom
             var count = s_GameModules.Count;
             for (var i = 0; i < count; i++)
             {
-                s_GameModules[i].LateUpdate();
+                try
+                {
+                    s_GameModules[i].LateUpdate();
+                }
+                catch (Exception e)
+                {
+#if UNITY_EDITOR
+                    UnityEngine.Debug.LogException(e);
+#endif
+                }
             }
         }
     }
