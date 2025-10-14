b250cd9acb485cddf198b24097fdc77df6ce747d
haloman9527 <haloman9527@qq.com>
2025-10-14 16:10:56 +0800

    add: 增加事件队列

---
+++ Atom.EventQueue/EventQueueStation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Atom
+{
+    public sealed class EventQueueStation
+    {
+        private Queue<QueueEventBase> m_EventQueue = new  Queue<QueueEventBase>();
+
+        public void Update()
+        {
+            if (m_EventQueue.Count > 0)
+            {
+                var cur = m_EventQueue.Peek();
+                if (cur.Completed)
+                {
+                    m_EventQueue.Dequeue();
+                    if (m_EventQueue.Count > 0)
+                    {
+                        var next = m_EventQueue.Peek();
+                        next.Invoke();
+                    }
+                }
+            }
+        }
+        
+        public void PushEvent(QueueEventBase evt)
+        {
+            m_EventQueue.Enqueue(evt);
+            if (m_EventQueue.Count == 1)
+                evt.Invoke();
+        }
+
+        public void Clear()
+        {
+            if (m_EventQueue.Count > 0)
+            {
+                var cur = m_EventQueue.Peek();
+                cur.Abort();
+            }
+            
+            m_EventQueue.Clear();
+        }
+    }
+}
---
+++ Atom.EventQueue/QueueEventBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Atom
+{
+    public abstract class QueueEventBase
+    {
+        public abstract bool Completed { get; }
+
+        public abstract void Invoke();
+
+        public abstract void Abort();
+    }
+}
